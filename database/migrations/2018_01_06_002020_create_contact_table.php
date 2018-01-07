49e7c2025274851793935c2e79056afb60b95047
rmorris <ryanmorris1232@gmail.com>
2018-01-07 17:23:04 -0600

    Correct table name

--- database/migrations/2018_01_06_002020_create_contact_table.php
+++ database/migrations/2018_01_06_002020_create_contact_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateContactsTable extends Migration
+class CreateContactTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateContactsTable extends Migration
      */
     public function up()
     {
-        Schema::create('contacts', function (Blueprint $table) {
+        Schema::create('contact', function (Blueprint $table) {
             $table->increments('id');
             $table->string('full_name');
             $table->string('email');
@@ -30,6 +30,6 @@ class CreateContactsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('contacts');
+        Schema::dropIfExists('contact');
     }
 }
